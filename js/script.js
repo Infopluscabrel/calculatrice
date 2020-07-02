b52084c4671d73c92bcedb6fbf019797df63eedf
geoffreyeukin <eukingeoffrey@gmail.com>
2020-07-02 11:35:49 +0100

    ameliorations

--- js/script.js
+++ js/script.js
@@ -43,6 +43,7 @@ function number(value){
 function reset() {
    printe.textContent = '0';
    sign.textContent = ',';
+   signee.textContent="0";
 }
 
 function signing() {
@@ -58,53 +59,26 @@ var number_boutons = document.querySelectorAll('.left_side .number li');
 
 var temp=0,print_value;
 
-function calcul(signe) {
-   
-   if(signed){
-      print_value = parseInt(print.innerHTML)* -1
-   }
-   else{
-       print_value = parseInt(print.innerHTML)
-   }
-   switch (signe) {
-      case '+':
-         
-         temp = addition(temp,print_value)
-         break;
-      case '-':
-         addition(temp,print_value)
-         break;
-      case '*':
-         addition(temp,print_value)
-         break;
-      case '/':
-         addition(temp,print_value)
-         break;
-      case '=':
-         print.innerHTML = temp
-         break;
 
-      default:
-         break;
-   }
-}
 function cosinus(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+var angle;   
+  var  x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.cos(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
-
 }
 function arccosninus(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+  var x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.acos(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
-   
+
 }function arcsinus(){
-   var angle=parseFloat(printe.textcontent);
+   angle=parseFloat(printe.textcontent)
    var angles=angle*Math.PI/180;
    angles=Math.asin(angles);
    angles=Math.fround(angles);
@@ -112,69 +86,79 @@ function arccosninus(){
    
 }
 function artangente(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+   var x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.atan(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
-   
+
 }
 
 function sinus(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+  var x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.sin(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
+
    
 }
 
 function cosinushyp(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+  var x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.cosh(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
-   
+
 }
 function logarithme(){
-   var angle=parseFloat(printe.textcontent);
-   angle= Math.logarithme(angle);
-   printe.textContent=angles;
-   
+  var x=printe.textContent
+  x=parseFloat(x);
+  var result=Math.log(x)
+  printe.textContent=result;
 }
 function exponentielle(){
-   var angle=parseFloat(printe.textcontent);
-        angle=Math.exp(angle);
+   var x=printe.textContent
+    x=parseFloat(x);
+
+   var angles=Math.exp(x);
+   printe.textContent=angles
 }
 
 function sinushyp(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+   x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.sinh(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
+
    
 }
 function tangente(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
+  var x=printe.textContent
+    x=parseFloat(x);
+   var angles=x*Math.PI/180;
    angles=Math.tan(angles);
    angles=Math.fround(angles);
    printe.textContent=angles;
-   
+
 }
 function tangentehyp(){
-   var angle=parseFloat(printe.textcontent);
-   var angles=angle*Math.PI/180;
-   angles=Math.tanh(angles);
-   angles=Math.fround(angles);
-   printe.textContent=angles;
-   
+  var x=printe.textContent
+   x=parseFloat(x);
+  var angles=x*Math.PI/180;
+  angles=Math.tanh(angles);
+  angles=Math.fround(angles);
+  printe.textContent=angles;
+
 }
 function addition(){
    var add=printe.textContent;
-   alert(add);
    signe="+";
    sign.textContent=signe;
    signee.textContent=add;
